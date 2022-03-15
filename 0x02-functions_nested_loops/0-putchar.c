1559ed34d7a096f5863f9111b3ad9344ef774af8
Amanuel <amansisay21@gmail.com>
2022-03-15 16:15:30 +0300

    the first commit'

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#!/bin/bash 
+#include "main.h"
+/*
+ *
+ * print _putchar,followed by a new line.
+ */
+int main(void)
+{
+	printf("_putchar");
+	return(0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+this is the _putchar program
+
